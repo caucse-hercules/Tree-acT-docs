33fbd3893dcef411d43f039bf614d03195b0b9c5
DoLim <limdongyoung0@gmail.com>
2021-06-03 18:57:24 +0900

    feat: init with config

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 const config = {
   gatsby: {
     pathPrefix: '/',
-    siteUrl: 'https://hasura.io',
+    siteUrl: 'https://treeact.cc',
     gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
-    title:
-      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    logo:
+      'https://pixabay.com/get/gea11c7bce51b55baee0c11fc281f6de9e436dba1818ab4b0a4a342adc5dfec0eadade0888616d883b3b9dd18eba052f0.svg',
+    logoLink: 'https://treeact.cc',
+    title: 'Tree-acT DOCS',
+    githubUrl: 'https://github.com/caucse-hercules/Tree-acT',
     helpUrl: '',
     tweetText: '',
-    social: `<li>
-		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
-		      <div class="twitterBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
-		      </div>
-		    </a>
-		  </li>
-			<li>
-		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
-		      <div class="discordBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
-		      </div>
-		    </a>
-		  </li>`,
+    // social: `<li>
+    //     <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+    //       <div class="twitterBtn">
+    //         <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+    //       </div>
+    //     </a>
+    //   </li>
+    // 	<li>
+    //     <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+    //       <div class="discordBtn">
+    //         <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+    //       </div>
+    //     </a>
+    //   </li>`,
     links: [{ text: '', link: '' }],
     search: {
       enabled: false,
@@ -44,18 +44,19 @@ const config = {
     collapsedNav: [
       '/codeblock', // add trailing slash if enabled above
     ],
-    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+    links: [{ text: 'Get Tree-acT', link: 'https://github.com/caucse-hercules/Tree-acT' }],
     frontline: false,
     ignoreIndex: true,
-    title:
-      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+    // title:
+    //   "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
   },
   siteMetadata: {
-    title: 'Gatsby Gitbook Boilerplate | Hasura',
-    description: 'Documentation built with mdx. Powering hasura.io/learn ',
+    title: 'Tree-acT | Generate your react project with Tree',
+    description: 'Learn how to use Tree-acT',
     ogImage: null,
-    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
-    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+    docsLocation: 'https://github.com/caucse-hercules/Tree-acT-page/tree/master/content',
+    favicon:
+      'https://pixabay.com/get/g18ad6eda480065d06cfb869112b70fc95928e43ad6df1303c6e3d6127ce5dab57a1ca9fca19758bb34813157b04e9b66.svg',
   },
   pwa: {
     enabled: false, // disabling this will also remove the existing service worker.
--- src/custom/styles/styles.js
+++ src/custom/styles/styles.js
@@ -1,7 +1,11 @@
 import { css } from '@emotion/react';
 
 const customStyles = css`
-
+  .navBarBrand img {
+    width: 60px;
+    margin-right: 6px;
+    display: inline-block;
+  }
 `;
 
 export const styles = [customStyles];
